a275dd3837521642ba16b7423b4e5a366d712c75
Daniel Maurer <maurer.da@outlook.com>
2019-10-15 15:19:06 +0200

    edited App.xaml.cs so startup can work

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using App1.Core.Services;
 using App1.Services;
-
+using App1.Views;
+using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace App1
 {
@@ -18,22 +21,57 @@ namespace App1
 
         public App()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
 
             // Deferred execution until used. Check https://msdn.microsoft.com/library/dd642331(v=vs.110).aspx for further info on Lazy<T> class.
             _activationService = new Lazy<ActivationService>(CreateActivationService);
 
             SampleDataService.InitializeDatabase();
+            this.Suspending += OnSuspending;
         }
 
-        protected override async void OnLaunched(LaunchActivatedEventArgs args)
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            if (!args.PrelaunchActivated)
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // App-Initialisierung nicht wiederholen, wenn das Fenster bereits Inhalte enthält.
+            // Nur sicherstellen, dass das Fenster aktiv ist.
+            if (rootFrame == null)
             {
-                await ActivationService.ActivateAsync(args);
+                // Frame erstellen, der als Navigationskontext fungiert und zum Parameter der ersten Seite navigieren
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Zustand von zuvor angehaltener Anwendung laden
+                }
+
+                // Den Frame im aktuellen Fenster platzieren
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // Wenn der Navigationsstapel nicht wiederhergestellt wird, zur ersten Seite navigieren
+                    // und die neue Seite konfigurieren, indem die erforderlichen Informationen als Navigationsparameter
+                    // übergeben werden
+                    rootFrame.Navigate(typeof(ImageGalleryPage), e.Arguments);
+                }
+                // Sicherstellen, dass das aktuelle Fenster aktiv ist
+                Window.Current.Activate();
             }
         }
 
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+        
         protected override async void OnActivated(IActivatedEventArgs args)
         {
             await ActivationService.ActivateAsync(args);
@@ -47,6 +85,13 @@ namespace App1
         private UIElement CreateShell()
         {
             return new Views.ShellPage();
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Anwendungszustand speichern und alle Hintergrundaktivitäten beenden
+            deferral.Complete();
         }
     }
 }
